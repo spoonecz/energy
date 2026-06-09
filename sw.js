7ea09146d13a1bae506695cc28e6e7856bc309ec
spoonecz <spoonecz@gmail.com>
2026-06-09 20:57:46 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'energytrack-v1';
+const FILES = [
+  '/energy/',
+  '/energy/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
